9552c69e5fb1a1d479fc9009651e57703ccfc7c4
M-Hatch <montehatch@gmail.com>
2015-11-22 12:11:10 -0500

    clean comments - working REST api

--- src/main/java/com/montgomeryhatch/converters/UserConverter.java
+++ src/main/java/com/montgomeryhatch/converters/UserConverter.java
@@ -1,12 +1,6 @@
 package com.montgomeryhatch.converters;
 
-//import java.util.ArrayList;
-//import java.util.List;
-
 import org.bson.types.ObjectId;
-
-//import com.mongodb.BasicDBList;
-//import com.mongodb.BasicDBObject;
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBObject;
 import com.montgomeryhatch.models.User;
@@ -22,16 +16,6 @@ public class UserConverter {
                 .append("email", p.getEmail());
         if (p.getId() != null)
             builder = builder.append("_id", new ObjectId(p.getId()));
-        // here is the problem
-        /*if (p.getBooks() != null){
-        	String[] strList = p.getBooks();
-        	List<ObjectId> booksList = new ArrayList<ObjectId>();
-        	
-        	for(int i = 0; i < p.getBooks().length; i++){
-        		booksList.add(new ObjectId(strList[i]));
-        	}
-            builder = builder.append("books", booksList);
-        }*/
         
         return builder.get();
     }
@@ -45,16 +29,6 @@ public class UserConverter {
         p.setEmail((String) doc.get("email"));
         ObjectId id = (ObjectId) doc.get("_id");
         p.setId(id.toString());
-        // here is the problem
-        /*List<String> strBooksList = new ArrayList<>();
-        BasicDBList booksList = (BasicDBList) doc.get("books");
-        for (int i = 0; i < booksList.size(); i++){
-        	BasicDBObject bkObj = (BasicDBObject) booksList.get(i);
-        	strBooksList.add(bkObj.toString());
-        }
-        String[] books = new String[strBooksList.size()];
-        books = strBooksList.toArray(books);
-        p.setBooks(books);*/
         
         return p;
     } 
