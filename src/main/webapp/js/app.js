6172a0a12c868f25124ca5441d852328ebdcef20
M-Hatch <montehatch@gmail.com>
2015-11-22 22:17:35 -0500

    extract search service and add authors and courses searches

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -12,18 +12,18 @@ app.config(function($routeProvider, $locationProvider){
     activetab: 'home'
   })
   .when('/titles', {
-    templateUrl: 'views/titles.html',
-    controller: 'titlesCtrl',
+    templateUrl: 'views/searches.html',
+    controller: 'titleCtrl',
     activetab: 'titles'
   })
   .when('/authors', {
-    templateUrl: 'views/authors.html',
-    controller: 'authorsCtrl',
+    templateUrl: 'views/searches.html',
+    controller: 'authorCtrl',
     activetab: 'authors'
   })
   .when('/courses', {
-    templateUrl: 'views/courses.html',
-    controller: 'coursesCtrl',
+    templateUrl: 'views/searches.html',
+    controller: 'courseCtrl',
     activetab: 'courses'
   })
   .when('/account', {
@@ -69,44 +69,82 @@ app.directive('dynamicHeight', ['$window', function($window) {
     }
 }]);
 
-app.controller('titlesCtrl', function($scope, $http, $route) {
+//search service
+app.service('SearchService', function(){
+	this.filtered_result = function(input, query, field) {
+	    // returns nothing if nothing in query box
+	    if (!query) return '';
+	
+	    //split query terms by space character
+	    var terms = query.split(' ');
+	    var output = [];
+	
+	    // iterate through input array
+	    input.forEach(function(product){
+	      var found = false;
+	      passTest = true;
+	
+	      // iterate through terms found in query box
+	      terms.forEach(function(term){
+	       
+	        // if all terms are found set boolean to true
+	    	if(field == "title")
+	    		found = (product.title.toLowerCase().indexOf(term.toLowerCase()) > -1);
+	    	if(field == "author")
+	    		found = (product.author.toLowerCase().indexOf(term.toLowerCase()) > -1);
+	    	if(field == "course")
+	    		found = (product.dept.toLowerCase().indexOf(term.toLowerCase()) > -1)
+	    		|| (product.course.toLowerCase().indexOf(term.toLowerCase()) > -1);
+	        
+	        passTest = passTest && found;
+	      });
+	
+	      // Add product to output array only if passTest is true -- all search terms were found in product
+	      if (passTest) { output.push(product); }
+	    });
+	
+	    return output;
+	  }
+});
+
+app.controller('titleCtrl', function($scope, $http, $route, SearchService) {
   $scope.books = null;
+  $scope.placeholder = "search titles...";
   $http.get('http://localhost/textbookswap/webapi/books')
   .success(function(response){
 	  $scope.allbooks = response;
   });
-  
-  $scope.filtered_result = function(input, query) {
-    // returns nothing if nothing in query box
-    if (!query) return '';
-
-    //split query terms by space character
-    var terms = query.split(' ');
-    var output = [];
-
-    // iterate through input array
-    input.forEach(function(product){
-      var found = false;
-      passTest = true;
-
-      // iterate through terms found in query box
-      terms.forEach(function(term){
-       
-        // if all terms are found set boolean to true
-        found = (product.title.toLowerCase().indexOf(term.toLowerCase()) > -1);
-        
-        passTest = passTest && found;
-      });
-
-      // Add product to output array only if passTest is true -- all search terms were found in product
-      if (passTest) { output.push(product); }
-    });
-
-    return output;
-  }
   // filter results on button click
   $scope.search = function(){
-	  $scope.books = $scope.filtered_result($scope.allbooks, $scope.query);
+	  $scope.books = SearchService.filtered_result($scope.allbooks, $scope.query, "title");
   }
   $scope.$route = $route;
 });
+
+app.controller('authorCtrl', function($scope, $http, $route, SearchService) {
+  $scope.books = null;
+  $scope.placeholder = "search authors...";
+  $http.get('http://localhost/textbookswap/webapi/books')
+  .success(function(response){
+	  $scope.allbooks = response;
+  });
+  // filter results on button click
+  $scope.search = function(){
+	  $scope.books = SearchService.filtered_result($scope.allbooks, $scope.query, "author");
+  }
+  $scope.$route = $route;
+});
+
+app.controller('courseCtrl', function($scope, $http, $route, SearchService) {
+	  $scope.books = null;
+	  $scope.placeholder = "search dept/course...";
+	  $http.get('http://localhost/textbookswap/webapi/books')
+	  .success(function(response){
+		  $scope.allbooks = response;
+	  });
+	  // filter results on button click
+	  $scope.search = function(){
+		  $scope.books = SearchService.filtered_result($scope.allbooks, $scope.query, "course");
+	  }
+	  $scope.$route = $route;
+	});
